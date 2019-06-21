eea98d406c65cdf81c48bafb04981b74ba03edec
warrenh <warrenh@bqool.com>
2019-06-21 11:20:50 +0800

    Revert "test"
    This reverts commit f23e0cebd76d4a287b7707c9c1360d06e9ae9ca3.

--- source/TodoMVC.Web/App_Start/RouteConfig.cs
+++ source/TodoMVC.Web/App_Start/RouteConfig.cs
@@ -18,8 +18,6 @@ namespace TodoMVC.Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-            var a = "aaaa";
         }
     }
 }
